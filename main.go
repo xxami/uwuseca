8afb4a6d6087d427ad200fdd4806b729f6c58c4c
ｌｉｌａｈ ▸ 🎀 <xxami@users.noreply.github.com>
2018-09-07 22:43:08 +0100

    draw a sprite

--- main.go
+++ main.go
@@ -2,16 +2,40 @@
 package main
 
 import (
+  "fmt"
+  _ "image/png"
+
   "github.com/hajimehoshi/ebiten"
   "github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
+const (
+  screen_width = 1024
+  screen_height = 720
+  screen_scale = 1
+  window_title = "uwuseca"
+)
+
+var (
+  image *ebiten.Image
+  image_options = &ebiten.DrawImageOptions{}
+)
+
+func init() {
+  img, _, err := ebitenutil.NewImageFromFile("res/note.png", ebiten.FilterDefault)
+  if err != nil {
+    panic(err)
+  }
+  image = img
+}
+
 func update(screen *ebiten.Image) error {
-  ebitenutil.DebugPrint(screen, "Hello, world!")
+  //ebitenutil.DebugPrint(screen, fmt.Sprintf("%0.2f", ebiten.CurrentFPS()))
+  screen.DrawImage(image, image_options)
   return nil
 }
 
 func main() {
-  ebiten.Run(update, 320, 240, 2, "Hello, world!")
+  ebiten.Run(update, screen_width, screen_height, screen_scale, window_title)
 }
 
