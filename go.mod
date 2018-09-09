ccdafb9391e1440c98df7006313fb6cd2efdcc51
ｌｉｌａｈ ▸ 🎀 <xxami@users.noreply.github.com>
2018-09-09 22:45:21 +0100

    add ttf font draw example

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/xxami/uwuseca
 
-require github.com/hajimehoshi/ebiten v1.7.3
+require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
+	github.com/hajimehoshi/ebiten v1.7.3
+	golang.org/x/image v0.0.0-20180708004352-c73c2afc3b81
+)
--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 
 import (
   "fmt"
+  "io/ioutil"
+  "image/color"
   _ "image/png"
 
+  "github.com/golang/freetype/truetype"
+  "golang.org/x/image/font"
   "github.com/hajimehoshi/ebiten"
+  "github.com/hajimehoshi/ebiten/text"
   "github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
@@ -19,6 +24,7 @@ const (
 var (
   image *ebiten.Image
   image_options = &ebiten.DrawImageOptions{}
+  debug_font font.Face
 )
 
 func init() {
@@ -27,11 +33,31 @@ func init() {
     panic(err)
   }
   image = img
+
+  font_file, err := ebitenutil.OpenFile("res/NotoSans-Regular.ttf")
+  if err != nil {
+    panic(err)
+  }
+  defer font_file.Close()
+  font_contents, err := ioutil.ReadAll(font_file)
+  if err != nil {
+    panic(err)
+  }
+  tt, err := truetype.Parse(font_contents)
+  if err != nil {
+    panic(err)
+  }
+  debug_font = truetype.NewFace(tt, &truetype.Options{
+    Size: 32,
+    DPI: 72,
+    Hinting: font.HintingFull,
+  })
 }
 
 func update(screen *ebiten.Image) error {
-  //ebitenutil.DebugPrint(screen, fmt.Sprintf("%0.2f", ebiten.CurrentFPS()))
   screen.DrawImage(image, image_options)
+  ebitenutil.DebugPrint(screen, fmt.Sprintf("%0.2f", ebiten.CurrentFPS()))
+  text.Draw(screen, "Hello, World!", debug_font, 20, 40, color.White)
   return nil
 }
 
